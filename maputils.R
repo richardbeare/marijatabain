d27f32469219675c7328f9b54518325c2b13b1ae
Richard Beare <Richard.Beare@gmail.com>
2016-07-19 12:19:38 +1000

    modified Marijas page

--- maputils.R
+++ maputils.R
@@ -1,6 +1,7 @@
 library(leaflet)
 library(ggmap)
-
+library(tidyr)
+library(dplyr)
 geocodeL <- function(ipapubs)
 {
   locations <- t(sapply(ipapubs$Address, geocode))
@@ -23,16 +24,44 @@ createPopupText <- function(language, pub)
   return(popup)
 }
 
-theMap <- function(ipapubs, type="a")
+## icons
+mI <- function(iconfile) {
+    makeIcon(
+      iconUrl = iconfile,
+      shadowUrl = "./icons/marker-shadow.png",
+      iconAnchorX=13,
+      iconAnchorY=40,
+      shadowAnchorX=13,
+      shadowAnchorY=40,
+      popupAnchorX =1,
+      popupAnchorY = -40
+    )
+}
+
+these.icons <- list(green=mI("icons/green_pin.png"),
+                    purple= mI("icons/purple_pin.png"),
+                    yellow= mI("icons/yellow_pin.png"),
+                    cyan=mI("icons/cyan_pin.png"))
+
+
+theMap <- function(ipapubs, type="a", icons=these.icons)
 {
   m <- leaflet()
-  
+  ipapubs <- within(ipapubs, {
+      Pubtype[is.na(Pubtype)] <- "standard"
+  }
+  )
   m <- switch(type, a=addTiles(m, group = "OSM (default)"),
               b =addProviderTiles(m, "OpenTopoMap", group="MapQuestOpen.Aerial"),
               c = addProviderTiles(m, "Esri.WorldTopoMap", group="Esri.WorldTopoMap"),
               d = addProviderTiles(m, "Esri.WorldImagery", group="Esri.WorldImagery"),
               e = addProviderTiles(m, "Esri.WorldPhysical", group="Esri.WorldPhysical")
   )
-  m <- addMarkers(m, lng=ipapubs$lon, lat=ipapubs$lat, popup=ipapubs$popup)
+  ii <- nest(group_by(ipapubs, Pubtype))
+  for (k in 1:length(ii$data)) {
+      iid <- ii$data[[k]]
+      m <- addMarkers(m, lng=iid$lon, lat=iid$lat, popup=iid$popup, icon=these.icons[[k]])
+  }
   return(m)
 }
+
