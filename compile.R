c5bb554ad3265515071e40276faa48aabb67c033
Richard Beare <richard.beare@monash.edu>
2017-05-24 19:01:50 +1000

    force use of rstudio pandoc

--- compile.R
+++ compile.R
@@ -1,10 +1,11 @@
 set.seed(0102)
 options(htmlwidgets.TOJSON_ARGS = list(pretty = TRUE))
-Sys.setenv(R_KNITR_OPTIONS = 'knitr.chunk.tidy = FALSE')
+Sys.setenv(R_KNITR_OPTIONS = 'knitr.chunk.tidy = FALSE', RSTUDIO_PANDOC='/usr/lib/rstudio/bin/pandoc')
 ##knitr::opts_chunk$set(out.width = '100%')
 library(leaflet)
+library(knitr)
+library(rmarkdown)
 f = rmarkdown::render(commandArgs(TRUE))
-
 
 
 
